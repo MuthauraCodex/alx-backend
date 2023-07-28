2594894e76c9162b9ff1bd846bb33cf482411a57
MuthauraCodex <gwenivymuthaura@gmail.com>
2023-07-28 01:11:09 +0000

    0x01-caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,53 +4,51 @@ LFUCache that inherits from BaseCaching
 """
 
 from base_caching import BaseCaching
+from collections import defaultdict
 
 
 class LFUCache(BaseCaching):
-    """
-    Most Recently Used (MRU) caching system implementation.
-    """
+    """ LFUCache class that inherits from BaseCaching """
+
     def __init__(self):
-        """
-        Initialize an LRUCache instance
-        """
+        """ Initialize """
         super().__init__()
-        self.cache_order = []
+        self.frequency = defaultdict(int)
+        self.min_frequency = 0
+        self.order = []
+
+    def update_frequency(self, key):
+        """ Update frequency of the key """
+        self.frequency[key] += 1
+        if self.frequency[key] > self.min_frequency:
+            self.min_frequency = self.frequency[key]
 
     def put(self, key, item):
-        """
-        Get an item by using its key and return it. If the key is not in the
-        cache, add it. If the key is in the cache, remove it and add it again.
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lfu_keys = [
+                    k for k, v in self.frequency.items()
+                    if v == self.min_frequency
+                ]
 
-        Args:
-            key: The key of the item to get.
-            item: The item to add to the cache.
+                if not lfu_keys:
+                    # In case all keys are already removed from the cache
+                    lfu_keys = self.order
 
-        Returns:
-            The item if it is in the cache, None otherwise.
-        """
-        if key and item:
+                lru_key = next(k for k in self.order if k in lfu_keys)
+                self.order.remove(lru_key)
+                del self.cache_data[lru_key]
+                del self.frequency[lru_key]
+                print("DISCARD: {}".format(lru_key))
+
             self.cache_data[key] = item
-            if key not in self.cache_order:
-                self.cache_order.append(key)
-            else:
-                self.cache_order.remove(key)
-                self.cache_order.append(key)
-            if len(self.cache_order) > BaseCaching.MAX_ITEMS:
-                popped = self.cache_order.pop(0)
-                del self.cache_data[popped]
-                print("DISCARD: {}".format(str(popped)))
+            self.update_frequency(key)
+            self.order.append(key)
 
     def get(self, key):
-        """
-        Get an item by using its key and return it. If the key is not
-        in the cache, return None.
-
-        Args:
-            key: The key of the item to get.
-        """
-        if key in self.cache_data:
-            self.cache_order.remove(key)
-            self.cache_order.append(key)
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            self.update_frequency(key)
             return self.cache_data[key]
         return None
